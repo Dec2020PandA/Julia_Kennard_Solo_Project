d19ad00500ee6a94cfa1dc13b994212fba49b467
Julia Kennard <juliabkennard@gmail.com>
2020-12-01 17:23:06 -0800

    adding gitnore

--- orderly_proj/orderly_proj/settings.py
+++ orderly_proj/orderly_proj/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from .secrets import secret_key
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*j3*_6)u5h=vatvk_pn598b3z1=kozrx06$3i8zbw2)##g5o3%'
+SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
